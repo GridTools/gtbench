5fd8358d063427291c7cd5e9bcbe744aa4aa13cf
Felix Thaler <thaler@cscs.ch>
2019-11-27 14:45:12 +0100

    basic benchmark app

---
+++ gt/benchmark.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include "./communication/simple_mpi.hpp"
+#include "./numerics/solver.hpp"
+#include "./verification/run.hpp"
+
+int main(int argc, char **argv) {
+  auto comm_world = communication::simple_mpi::world(argc, argv);
+
+  if (argc != 2) {
+    std::cerr << "usage: " << argv[0] << " N" << std::endl;
+    return 1;
+  }
+
+  const std::size_t n = std::atoll(argv[1]);
+  const std::size_t nz = 100;
+
+  std::cout << "running benchmark with " << n << "x" << n << " columns"
+            << std::endl;
+
+  auto comm_grid = communication::grid(comm_world, {n, n, 100});
+
+  real_t diffusion_coeff = 0.05;
+  auto exact =
+      analytical::repeat(analytical::advection_diffusion{diffusion_coeff},
+                         {(n + nz - 1) / nz, (n + nz - 1) / nz, 1});
+
+  auto error =
+      run(std::move(comm_grid), full_stepper(diffusion_coeff), 1, 1e-3, exact);
+  std::cout << "error: " << error << std::endl;
+
+  return 0;
+}
