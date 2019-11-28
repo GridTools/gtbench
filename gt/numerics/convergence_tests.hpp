98d7a6db930fb06567262086d0169466dbca0128
Felix Thaler <thaler@cscs.ch>
2019-11-28 10:45:40 +0100

    cleanup

--- gt/numerics/convergence_tests.hpp
+++ gt/numerics/convergence_tests.hpp
@@ -9,102 +9,70 @@
 #include "./solver.hpp"
 
 template <class CommWorld> void run_convergence_tests(CommWorld &&comm_world) {
-  auto run_spatial = [&comm_world](auto &&exact, auto &&stepper) {
-    auto error_f = [&comm_world, exact = std::move(exact),
-                    stepper = std::move(stepper)](std::size_t resolution) {
+  auto run_tests = [&comm_world](auto &&exact, auto &&stepper) {
+    std::cout << "Spatial convergence:" << std::endl;
+    auto spatial_error_f = [&comm_world, exact = std::move(exact),
+                            stepper =
+                                std::move(stepper)](std::size_t resolution) {
       return run(communication::grid(comm_world,
                                      {resolution, resolution, resolution}),
                  stepper, 1e-5, 1e-6, exact)
           .error;
     };
-    print_order_verification_result(order_verification(error_f, 8, 128));
-  };
+    print_order_verification_result(
+        order_verification(spatial_error_f, 8, 128));
 
-  auto run_temporal = [&comm_world](auto &&exact, auto &&stepper) {
-    auto error_f = [&comm_world, exact = std::move(exact),
-                    stepper = std::move(stepper)](std::size_t resolution) {
+    std::cout << "Space-Time convergence:" << std::endl;
+    auto spacetime_error_f = [&comm_world, exact = std::move(exact),
+                              stepper =
+                                  std::move(stepper)](std::size_t resolution) {
       return run(communication::grid(comm_world,
                                      {resolution, resolution, resolution}),
                  stepper, 1e-4, 1e-5 / resolution, exact)
           .error;
     };
-    print_order_verification_result(order_verification(error_f, 8, 64));
+    print_order_verification_result(
+        order_verification(spacetime_error_f, 8, 64));
   };
 
+  const real_t diffusion_coeff = 0.05;
+
   {
-    std::cout << "HORIZONTAL DIFFUSION: Spatial Convergence" << std::endl;
-    analytical::horizontal_diffusion exact{0.05};
-    run_spatial(exact, hdiff_stepper(exact.diffusion_coeff));
-  }
-  {
-    std::cout << "HORIZONTAL DIFFUSION: Space-Time Convergence" << std::endl;
-    analytical::horizontal_diffusion exact{0.05};
-    run_temporal(exact, hdiff_stepper(exact.diffusion_coeff));
+    std::cout << "HORIZONTAL DIFFUSION" << std::endl;
+    run_tests(analytical::horizontal_diffusion{diffusion_coeff},
+              hdiff_stepper(diffusion_coeff));
   }
 
   {
-    std::cout << "VERTICAL DIFFUSION: Spatial Convergence" << std::endl;
-    analytical::vertical_diffusion exact{0.05};
-    run_spatial(exact, vdiff_stepper(exact.diffusion_coeff));
-  }
-  {
-    std::cout << "VERTICAL DIFFUSION: Space-Time Convergence" << std::endl;
-    analytical::vertical_diffusion exact{0.05};
-    run_temporal(exact, vdiff_stepper(exact.diffusion_coeff));
+    std::cout << "VERTICAL DIFFUSION" << std::endl;
+    run_tests(analytical::vertical_diffusion{diffusion_coeff},
+              vdiff_stepper(diffusion_coeff));
   }
 
   {
-    std::cout << "FULL DIFFUSION: Spatial Convergence" << std::endl;
-    analytical::full_diffusion exact{0.05};
-    run_spatial(exact, diff_stepper(exact.diffusion_coeff));
-  }
-  {
-    std::cout << "FULL DIFFUSION: Space-Time Convergence" << std::endl;
-    analytical::full_diffusion exact{0.05};
-    run_temporal(exact, diff_stepper(exact.diffusion_coeff));
+    std::cout << "FULL DIFFUSION" << std::endl;
+    run_tests(analytical::full_diffusion{diffusion_coeff},
+              diff_stepper(diffusion_coeff));
   }
 
   {
-    std::cout << "HORIZONTAL ADVECTION: Spatial Convergence" << std::endl;
-    analytical::horizontal_advection exact;
-    run_spatial(exact, hadv_stepper());
-  }
-  {
-    std::cout << "HORIZONTAL ADVECTION: Space-Time Convergence" << std::endl;
-    analytical::horizontal_advection exact;
-    run_temporal(exact, hadv_stepper());
+    std::cout << "HORIZONTAL ADVECTION" << std::endl;
+    run_tests(analytical::horizontal_advection{}, hadv_stepper());
   }
 
   {
-    std::cout << "VERTICAL ADVECTION: Spatial Convergence" << std::endl;
-    analytical::vertical_advection exact;
-    run_spatial(exact, vadv_stepper());
-  }
-  {
-    std::cout << "VERTICAL ADVECTION: Space-Time Convergence" << std::endl;
-    analytical::vertical_advection exact;
-    run_temporal(exact, vadv_stepper());
+    std::cout << "VERTICAL ADVECTION" << std::endl;
+    run_tests(analytical::vertical_advection{}, vadv_stepper());
   }
 
   {
-    std::cout << "RUNGE-KUTTA ADVECTION: Spatial Convergence" << std::endl;
-    analytical::full_advection exact;
-    run_spatial(exact, rkadv_stepper());
-  }
-  {
-    std::cout << "RUNGE-KUTTA ADVECTION: Space-Time Convergence" << std::endl;
-    analytical::full_advection exact;
-    run_temporal(exact, rkadv_stepper());
+    std::cout << "RUNGE-KUTTA ADVECTION" << std::endl;
+    run_tests(analytical::full_advection{}, rkadv_stepper());
   }
 
   {
     std::cout << "ADVECTION-DIFFUSION: Spatial Convergence" << std::endl;
-    analytical::advection_diffusion exact{0.05};
-    run_spatial(exact, full_stepper(exact.diffusion_coeff));
-  }
-  {
-    std::cout << "ADVECTION-DIFFUSION: Space-Time Convergence" << std::endl;
-    analytical::advection_diffusion exact{0.05};
-    run_temporal(exact, full_stepper(exact.diffusion_coeff));
+    run_tests(analytical::advection_diffusion{diffusion_coeff},
+              full_stepper(diffusion_coeff));
   }
 }
