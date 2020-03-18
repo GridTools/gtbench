f6b06446bab47d457ef044967e4b613b9e0ab5f0
Felix Thaler <thaler@cscs.ch>
2020-03-18 08:01:24 +0100

    Renamed variable

--- runtime/single_node/run.hpp
+++ runtime/single_node/run.hpp
@@ -29,10 +29,10 @@ template <class Analytical, class Stepper>
 result runtime_solve(single_node, Analytical analytical, Stepper stepper,
                      vec<std::size_t, 3> const &global_resolution, real_t tmax,
                      real_t dt) {
-  const auto initial = discrete_analytical::discretize(
+  const auto exact = discrete_analytical::discretize(
       analytical, global_resolution, global_resolution, {0, 0});
 
-  auto state = computation::init_state(initial);
+  auto state = computation::init_state(exact);
   auto exchange = single_node_impl::setup(global_resolution);
 
   auto step = stepper(state, exchange);
@@ -52,7 +52,7 @@ result runtime_solve(single_node, Analytical analytical, Stepper stepper,
 
   auto stop = clock::now();
   double time = std::chrono::duration<double>(stop - start).count();
-  double error = computation::compute_error(state, initial, t);
+  double error = computation::compute_error(state, exact, t);
 
   return {error, time};
 }
