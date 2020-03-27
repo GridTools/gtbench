d51622ae81203743364ba73cd69117518eb796a9
Felix Thaler <thaler@cscs.ch>
2020-03-27 09:59:55 +0100

    Fix typo in header message

--- runtime/discrete_analytical.hpp
+++ runtime/discrete_analytical.hpp
@@ -2,7 +2,7 @@
  * gtbench
  *
  * Copyright (c) 2014-2020, ETH Zurich
- * All rights reservedisc.
+ * All rights reserved.
  *
  * Please, refer to the LICENSE file in the root directory.
  * SPDX-License-Identifier: BSD-3-Clause
