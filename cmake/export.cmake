54ace235cab601c282b3b3f2cb9047a7684484e5
Felix Thaler <thaler@cscs.ch>
2020-11-25 10:01:56 +0100

    CMake installation & export

---
+++ cmake/export.cmake
@@ -0,0 +1,36 @@
+set(GTBENCH_MODULE_PATH lib/cmake/GTBench)
+include(GNUInstallDirs)
+include(CMakePackageConfigHelpers)
+configure_package_config_file(
+  cmake/GTBenchConfig.cmake.in
+  "${CMAKE_CURRENT_BINARY_DIR}/install/GTBenchConfig.cmake"
+  PATH_VARS GTBENCH_MODULE_PATH
+  INSTALL_DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/GTBench"
+)
+write_basic_package_version_file(
+  "${CMAKE_CURRENT_BINARY_DIR}/install/GTBenchConfigVersion.cmake"
+  COMPATIBILITY SameMajorVersion
+)
+
+install(
+  TARGETS gtbench stencil_${GTBENCH_BACKEND} storage_${GTBENCH_BACKEND}
+  EXPORT GTBenchTargets
+  INCLUDES DESTINATION include
+)
+install(TARGETS benchmark convergence_tests)
+install(DIRECTORY include/ DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+
+include(GNUInstallDirs)
+install(
+  FILES
+    "${CMAKE_CURRENT_BINARY_DIR}/install/GTBenchConfig.cmake"
+    "${CMAKE_CURRENT_BINARY_DIR}/install/GTBenchConfigVersion.cmake"
+  DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}"
+)
+
+install(
+  EXPORT GTBenchTargets
+  FILE GTBenchTargets.cmake
+  NAMESPACE GTBench::
+  DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}"
+)
