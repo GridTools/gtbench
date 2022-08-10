7b88bdc81a7018f90cc261ea69c09617ae397bd7
Felix Thaler <thaler@cscs.ch>
2022-08-10 14:27:49 +0200

    Fix vectorization for GCC 11+: don’t use GT expressions and calls

--- src/numerics/advection.cpp
+++ src/numerics/advection.cpp
@@ -20,75 +20,8 @@ namespace advection {
 namespace {
 using gt::stencil::extent;
 using gt::stencil::make_param_list;
-using gt::stencil::cartesian::call;
-using gt::stencil::cartesian::call_proc;
 using gt::stencil::cartesian::in_accessor;
 using gt::stencil::cartesian::inout_accessor;
-using namespace gt::stencil::cartesian::expressions;
-
-struct stage_u {
-  using flux = inout_accessor<0>;
-  using u = in_accessor<1>;
-  using in = in_accessor<2, extent<-3, 3, 0, 0>>;
-  using dx = in_accessor<3>;
-  using param_list = make_param_list<flux, u, in, dx>;
-
-  template <typename Evaluation>
-  GT_FUNCTION static void apply(Evaluation eval, full_t) {
-    static constexpr real_t weights[] = {1_r / 30, -1_r / 4, 1_r,
-                                         -1_r / 3, -1_r / 2, 1_r / 20};
-
-    if (eval(u()) > 0_r) {
-      eval(flux()) =
-          eval(u() *
-               -(weights[0] * in(-3, 0, 0) + weights[1] * in(-2, 0, 0) +
-                 weights[2] * in(-1, 0, 0) + weights[3] * in() +
-                 weights[4] * in(1, 0, 0) + weights[5] * in(2, 0, 0)) /
-               dx());
-    } else if (eval(u()) < 0_r) {
-      eval(flux()) =
-          eval(u() *
-               (weights[5] * in(-2, 0, 0) + weights[4] * in(-1, 0, 0) +
-                weights[3] * in() + weights[2] * in(1, 0, 0) +
-                weights[1] * in(2, 0, 0) + weights[0] * in(3, 0, 0)) /
-               dx());
-    } else {
-      eval(flux()) = 0_r;
-    }
-  }
-};
-struct stage_v {
-  using flux = inout_accessor<0>;
-  using v = in_accessor<1>;
-  using in = in_accessor<2, extent<0, 0, -3, 3>>;
-  using dy = in_accessor<3>;
-
-  using param_list = make_param_list<flux, v, in, dy>;
-
-  template <typename Evaluation>
-  GT_FUNCTION static void apply(Evaluation eval, full_t) {
-    static constexpr real_t weights[] = {1_r / 30, -1_r / 4, 1_r,
-                                         -1_r / 3, -1_r / 2, 1_r / 20};
-
-    if (eval(v()) > 0_r) {
-      eval(flux()) =
-          eval(v() *
-               -(weights[0] * in(0, -3, 0) + weights[1] * in(0, -2, 0) +
-                 weights[2] * in(0, -1, 0) + weights[3] * in() +
-                 weights[4] * in(0, 1, 0) + weights[5] * in(0, 2, 0)) /
-               dy());
-    } else if (eval(v()) < 0_r) {
-      eval(flux()) =
-          eval(v() *
-               (weights[5] * in(0, -2, 0) + weights[4] * in(0, -1, 0) +
-                weights[3] * in() + weights[2] * in(0, 1, 0) +
-                weights[1] * in(0, 2, 0) + weights[0] * in(0, 3, 0)) /
-               dy());
-    } else {
-      eval(flux()) = 0_r;
-    }
-  }
-};
 
 struct stage_horizontal {
   using out = inout_accessor<0>;
@@ -105,10 +38,46 @@ struct stage_horizontal {
 
   template <typename Evaluation>
   GT_FUNCTION static void apply(Evaluation eval, full_t) {
-    auto flx = call<stage_u, full_t>::with(eval, u(), in(), dx());
-    auto fly = call<stage_v, full_t>::with(eval, v(), in(), dy());
+    static constexpr real_t weights[] = {1_r / 30, -1_r / 4, 1_r,
+                                         -1_r / 3, -1_r / 2, 1_r / 20};
 
-    eval(out()) = eval(in0() - dt() * (flx + fly));
+    real_t flx_x, flx_y;
+    if (eval(u()) > 0_r) {
+      flx_x =
+          eval(u()) *
+          -(weights[0] * eval(in(-3, 0, 0)) + weights[1] * eval(in(-2, 0, 0)) +
+            weights[2] * eval(in(-1, 0, 0)) + weights[3] * eval(in()) +
+            weights[4] * eval(in(1, 0, 0)) + weights[5] * eval(in(2, 0, 0))) /
+          eval(dx());
+    } else if (eval(u()) < 0_r) {
+      flx_x =
+          eval(u()) *
+          (weights[5] * eval(in(-2, 0, 0)) + weights[4] * eval(in(-1, 0, 0)) +
+           weights[3] * eval(in()) + weights[2] * eval(in(1, 0, 0)) +
+           weights[1] * eval(in(2, 0, 0)) + weights[0] * eval(in(3, 0, 0))) /
+          eval(dx());
+    } else {
+      flx_x = 0_r;
+    }
+    if (eval(v()) > 0_r) {
+      flx_y =
+          eval(v()) *
+          -(weights[0] * eval(in(0, -3, 0)) + weights[1] * eval(in(0, -2, 0)) +
+            weights[2] * eval(in(0, -1, 0)) + weights[3] * eval(in()) +
+            weights[4] * eval(in(0, 1, 0)) + weights[5] * eval(in(0, 2, 0))) /
+          eval(dy());
+    } else if (eval(v()) < 0_r) {
+      flx_y =
+          eval(v()) *
+          (weights[5] * eval(in(0, -2, 0)) + weights[4] * eval(in(0, -1, 0)) +
+           weights[3] * eval(in()) + weights[2] * eval(in(0, 1, 0)) +
+           weights[1] * eval(in(0, 2, 0)) + weights[0] * eval(in(0, 3, 0))) /
+          eval(dy());
+    } else {
+      flx_y = 0_r;
+    }
+
+    eval(out()) = eval(in0()) - eval(dt()) * (flx_x + flx_y);
   }
 };
 
@@ -132,12 +101,14 @@ struct stage_advection_w_forward {
   GT_FUNCTION static void apply(Evaluation eval, full_t::first_level) {
     auto k_offset = eval(k_size()) - 1;
 
-    real_t av = eval(-0.25_r / dz() * w());
-    real_t bv = eval(1_r / dt() + 0.25_r * (w() - w(0, 0, 1)) / dz());
-    real_t d1v = eval(1_r / dt() * in() -
-                      0.25_r / dz() *
-                          (w() * (in() - in_uncached(0, 0, k_offset)) +
-                           w(0, 0, 1) * (in(0, 0, 1) - in())));
+    real_t av = -0.25_r / eval(dz()) * eval(w());
+    real_t bv =
+        1_r / eval(dt()) + 0.25_r * (eval(w()) - eval(w(0, 0, 1))) / eval(dz());
+    real_t d1v =
+        1_r / eval(dt()) * eval(in()) -
+        0.25_r / eval(dz()) *
+            (eval(w()) * (eval(in()) - eval(in_uncached(0, 0, k_offset))) +
+             eval(w(0, 0, 1)) * (eval(in(0, 0, 1)) - eval(in())));
     real_t d2v = -av;
 
     eval(b()) = bv;
@@ -147,38 +118,40 @@ struct stage_advection_w_forward {
 
   template <typename Evaluation>
   GT_FUNCTION static void apply(Evaluation eval, full_t::modify<1, -2>) {
-    real_t av = eval(-0.25_r / dz() * w());
-    real_t bv = eval(1_r / dt() + 0.25_r * (w() - w(0, 0, 1)) / dz());
+    real_t av = -0.25_r / eval(dz()) * eval(w());
+    real_t bv =
+        1_r / eval(dt()) + 0.25_r * (eval(w()) - eval(w(0, 0, 1))) / eval(dz());
     real_t cv_km1 = -av;
-    real_t d1v =
-        eval(1_r / dt() * in() - 0.25_r / dz() *
-                                     (w() * (in() - in(0, 0, -1)) +
-                                      w(0, 0, 1) * (in(0, 0, 1) - in())));
+    real_t d1v = 1_r / eval(dt()) * eval(in()) -
+                 0.25_r / eval(dz()) *
+                     (eval(w()) * (eval(in()) - eval(in(0, 0, -1))) +
+                      eval(w(0, 0, 1)) * (eval(in(0, 0, 1)) - eval(in())));
     real_t d2v = 0_r;
 
-    real_t f = eval(av / b(0, 0, -1));
+    real_t f = av / eval(b(0, 0, -1));
     eval(b()) = bv - f * cv_km1;
-    eval(d1()) = eval(d1v - f * d1(0, 0, -1));
-    eval(d2()) = eval(d2v - f * d2(0, 0, -1));
+    eval(d1()) = d1v - f * eval(d1(0, 0, -1));
+    eval(d2()) = d2v - f * eval(d2(0, 0, -1));
   }
 
   template <typename Evaluation>
   GT_FUNCTION static void apply(Evaluation eval,
                                 full_t::last_level::shift<-1>) {
-    real_t av = eval(-0.25_r / dz() * w());
-    real_t bv = eval(1_r / dt() + 0.25_r * (w() - w(0, 0, 1)) / dz());
-    real_t cv = eval(0.25_r / dz() * w(0, 0, 1));
+    real_t av = -0.25_r / eval(dz()) * eval(w());
+    real_t bv =
+        1_r / eval(dt()) + 0.25_r * (eval(w()) - eval(w(0, 0, 1))) / eval(dz());
+    real_t cv = 0.25_r / eval(dz()) * eval(w(0, 0, 1));
     real_t cv_km1 = -av;
-    real_t d1v =
-        eval(1_r / dt() * in() - 0.25_r / dz() *
-                                     (w() * (in() - in(0, 0, -1)) +
-                                      w(0, 0, 1) * (in(0, 0, 1) - in())));
+    real_t d1v = 1_r / eval(dt()) * eval(in()) -
+                 0.25_r / eval(dz()) *
+                     (eval(w()) * (eval(in()) - eval(in(0, 0, -1))) +
+                      eval(w(0, 0, 1)) * (eval(in(0, 0, 1)) - eval(in())));
     real_t d2v = -cv;
 
-    real_t f = eval(av / b(0, 0, -1));
+    real_t f = av / eval(b(0, 0, -1));
     eval(b()) = bv - f * cv_km1;
-    eval(d1()) = eval(d1v - f * d1(0, 0, -1));
-    eval(d2()) = eval(d2v - f * d2(0, 0, -1));
+    eval(d1()) = d1v - f * eval(d1(0, 0, -1));
+    eval(d2()) = d2v - f * eval(d2(0, 0, -1));
   }
 };
 
@@ -195,17 +168,17 @@ struct stage_advection_w_backward {
   template <typename Evaluation>
   GT_FUNCTION static void apply(Evaluation eval,
                                 full_t::last_level::shift<-1>) {
-    auto f = eval(1_r / b());
+    auto f = 1_r / eval(b());
     eval(d1()) *= f;
     eval(d2()) *= f;
   }
 
   template <typename Evaluation>
   GT_FUNCTION static void apply(Evaluation eval, full_t::modify<0, -2>) {
-    auto cv = eval(0.25_r / dz() * w(0, 0, 1));
-    auto f = eval(1_r / b());
-    eval(d1()) = eval((d1() - cv * d1(0, 0, 1)) * f);
-    eval(d2()) = eval((d2() - cv * d2(0, 0, 1)) * f);
+    auto cv = 0.25_r / eval(dz()) * eval(w(0, 0, 1));
+    auto f = 1_r / eval(b());
+    eval(d1()) = (eval(d1()) - cv * eval(d1(0, 0, 1))) * f;
+    eval(d2()) = (eval(d2()) - cv * eval(d2(0, 0, 1))) * f;
   }
 };
 
@@ -227,27 +200,30 @@ struct stage_advection_w3 {
 
   template <typename Evaluation>
   GT_FUNCTION static void apply(Evaluation eval, full_t::last_level) {
-    auto k_offset = eval(k_size() - 1);
+    auto k_offset = eval(k_size()) - 1;
 
-    real_t av = eval(-0.25_r / dz() * w());
-    real_t bv = eval(1_r / dt() + 0.25_r * (w() - w(0, 0, 1)) / dz());
-    real_t cv = eval(0.25_r / dz() * w(0, 0, 1));
+    real_t av = -0.25_r / eval(dz()) * eval(w());
+    real_t bv =
+        1_r / eval(dt()) + 0.25_r * (eval(w()) - eval(w(0, 0, 1))) / eval(dz());
+    real_t cv = 0.25_r / eval(dz()) * eval(w(0, 0, 1));
 
-    real_t d1v = eval(1_r / dt() * in() -
-                      0.25_r / dz() *
-                          (w() * (in() - in(0, 0, -1)) +
-                           w(0, 0, 1) * (in(0, 0, -k_offset) - in())));
+    real_t d1v =
+        1_r / eval(dt()) * eval(in()) -
+        0.25_r / eval(dz()) *
+            (eval(w()) * (eval(in()) - eval(in(0, 0, -1))) +
+             eval(w(0, 0, 1)) * (eval(in(0, 0, -k_offset)) - eval(in())));
 
     eval(out_top()) =
-        eval((d1v - cv * d1(0, 0, -k_offset) - av * d1(0, 0, -1)) /
-             (bv + cv * d2(0, 0, -k_offset) + av * d2(0, 0, -1)));
-    eval(out()) = eval(in0() + (out_top() - in()));
+        (d1v - cv * eval(d1(0, 0, -k_offset)) - av * eval(d1(0, 0, -1))) /
+        (bv + cv * eval(d2(0, 0, -k_offset)) + av * eval(d2(0, 0, -1)));
+    eval(out()) = eval(in0()) + (eval(out_top()) - eval(in()));
   }
 
   template <typename Evaluation>
   GT_FUNCTION static void apply(Evaluation eval, full_t::modify<0, -1>) {
     eval(out_top()) = eval(out_top(0, 0, 1));
-    eval(out()) = eval(in0() + (d1() + d2() * out_top() - in()));
+    eval(out()) =
+        eval(in0()) + (eval(d1()) + eval(d2()) * eval(out_top()) - eval(in()));
   }
 };
 
