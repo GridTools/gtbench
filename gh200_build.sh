615fbacf87e5d5de37d973b640e92387d6fef87d
Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>
2024-02-26 22:10:58 +0100

    Fix indentation

--- gh200_build.sh
+++ gh200_build.sh
@@ -18,21 +18,20 @@ for backend in cpu_ifirst cpu_kfirst gpu; do
             for ghex_backend in MPI UCX LIBFABRIC; do
                 for xpmem in ON OFF; do
                     if [[ $xpmem == "ON" ]]; then
-			    XPMEM_CMAKE_FLAGS="-DGHEX_USE_XPMEM=ON -DXPMEM_DIR=/opt/cray/xpmem/default"
-		    else
-			    XPMEM_CMAKE_FLAGS=""
+                        XPMEM_CMAKE_FLAGS="-DGHEX_USE_XPMEM=ON -DXPMEM_DIR=/opt/cray/xpmem/default"
+                    else
+                        XPMEM_CMAKE_FLAGS=""
                     fi
                     if [[ $ghex_backend == "LIBFABRIC" ]]; then
-			    GHEX_CMAKE_OPTIONS="$GHEX_CMAKE_OPTIONS -DLIBFABRIC_DIR=/opt/cray/libfabric/1.15.2.0"
+			            GHEX_CMAKE_OPTIONS="$GHEX_CMAKE_OPTIONS -DLIBFABRIC_DIR=/opt/cray/libfabric/1.15.2.0"
                     fi
-		    BUILD_DIR=build_${backend}_${gt_runtime}_${ghex_backend}
-		    if [[ $xpmem == "ON" ]]; then
-			    BUILD_DIR="${BUILD_DIR}_xpmem"
-		    fi
-		    mkdir -p $BUILD_DIR
+                    BUILD_DIR=build_${backend}_${gt_runtime}_${ghex_backend}
+                    if [[ $xpmem == "ON" ]]; then
+                        BUILD_DIR="${BUILD_DIR}_xpmem"
+                    fi
+                    mkdir -p $BUILD_DIR
                     pushd $BUILD_DIR
                     set -x
-
                     cmake .. -DGTBENCH_BACKEND=$backend -DGTBENCH_RUNTIME=$gt_runtime -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_CXX_COMPILER=$(which g++) -DCMAKE_BUILD_TYPE=Custom -DCMAKE_CXX_FLAGS="-g -mcpu=neoverse-v2 -Ofast -msve-vector-bits=128 -fopt-info-vec-missed -fvect-cost-model=unlimited" -DCMAKE_CUDA_COMPILER=${CMAKE_CUDA_COMPILER} -DGHEX_TRANSPORT_BACKEND=$ghex_backend ${GHEX_CMAKE_OPTIONS} ${XPMEM_CMAKE_FLAGS}
                     set +x
                     cmake --build . --target install --parallel 1 --verbose 2>&1 | tee build_output.txt
@@ -41,7 +40,7 @@ for backend in cpu_ifirst cpu_kfirst gpu; do
             done
         else
             BUILD_DIR=build_${backend}_${gt_runtime}
-	    mkdir -p $BUILD_DIR
+	        mkdir -p $BUILD_DIR
             pushd $BUILD_DIR
             set -x
             cmake .. -DGTBENCH_BACKEND=$backend -DGTBENCH_RUNTIME=$gt_runtime -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_CXX_COMPILER=$(which g++) -DCMAKE_BUILD_TYPE=Custom -DCMAKE_CXX_FLAGS="-g -mcpu=neoverse-v2 -Ofast -msve-vector-bits=128 -fopt-info-vec-missed -fvect-cost-model=unlimited" -DCMAKE_CUDA_COMPILER=${CMAKE_CUDA_COMPILER}
